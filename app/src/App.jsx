7e14f90b5789ae305d5f4ad3bc049e0fd4123f8b
Aditya Krishnamurthi <krisa04@pfw.edu>
2024-11-12 19:08:59 -0500

    Sign-in module changes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SignIn from './pages/Signin.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <SignIn /> 
   )
 }
 
