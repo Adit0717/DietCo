0a6c9f116eb50909fea03bd4030b2509141bacde
Aditya Krishnamurthi <krisa04@pfw.edu>
2024-11-19 18:51:22 -0500

    dashboard changes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,13 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SignIn from './pages/Signin.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <SignIn /> 
-  )
+    <div>
+      {isLoggedIn ? (
+        <Dashboard />
+      ) : (
+        <SignIn />
+      )}
+      
+      <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+        {isLoggedIn ? 'Log Out' : 'Log In'}
+      </button>
+    </div>
+  );
 }
 
 export default App
